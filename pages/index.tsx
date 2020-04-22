fe0bdf8f110ccb29a3b60d1bc1fda0f3a0248fa8
James Porter <james@amimetic.co.uk>
2020-04-22 18:25:23 +0100

    actually use it to draw somethimg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,8 @@
 import Head from "next/head"
 import { useState, useEffect } from "react"
+import { SVGSketch } from "../src/components/SVGSketch"
 
 export default function Home() {
-  const [time, setTime] = useState(0)
-  useEffect(() => {
-    let i = 0
-    const iv = setInterval(() => {
-      setTime(i++)
-    }, 1000)
-    return () => clearInterval(iv)
-  }, [])
-
   return (
     <div className="container">
       <Head>
@@ -18,8 +10,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <img
-        src={`data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100' width='200' height='200'><rect x='${time}' y='${time}' width='10' height='10' /></svg>`}
+      <SVGSketch
+        width={480}
+        height={480}
+        sketch={(s) => {
+          const path = s
+            .strokedPath((attr) =>
+              attr.stroke(220, 90, 30, 0.9).fill(20, 90, 80, 0.2)
+            )
+            .moveTo(s.randomPoint())
+          s.times(25, () => {
+            path.lineTo(s.randomPoint())
+          })
+        }}
       />
     </div>
   )
---
+++ src/components/SVGSketch.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { SolandraSvg } from "../lib/index"
+
+export function SVGSketch({
+  sketch,
+  width,
+  height,
+}: {
+  sketch: (sol: SolandraSvg) => void
+  width
+  height
+}) {
+  const svg = new SolandraSvg(width, height, 1)
+  sketch(svg)
+  const image = svg.image
+
+  return (
+    <img src={`data:image/svg+xml;utf8,${image}`} style={{ width, height }} />
+  )
+}
